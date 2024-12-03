0b4c94680ad24d3b0810a1259a6d454dddcb0a2f
abrahan villa <ajvilla1999@gmail.com>
2024-12-03 13:37:40 -0500

    add speed-test v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { AppRouter } from "./router/AppRouter";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
+      <SpeedInsights />
       <AppRouter/> 
     </>
   );
